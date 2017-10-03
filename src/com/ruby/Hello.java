8c22619fa2b6f42d615d7b3b22778ae41b314dce
ruby <ruby10387@gamil.com>
2017-10-03 16:39:05 +0800

    initial commit

---
+++ src/com/ruby/Hello.java
@@ -0,0 +1,12 @@
+package com.ruby;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		Person p = new Person();
+		p.sayhello();
+		}
+
+}
---
+++ src/com/ruby/Person.java
@@ -0,0 +1,13 @@
+package com.ruby;
+
+public class Person {
+	public void sayhello(){
+		System.out.println("Hello!");
+	}
+	public static void main(String[] args) {
+
+		Person p = new Person();
+		p.sayhello();
+		}
+
+}
