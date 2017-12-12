b5127d560c6ac50887b1d421dc30a4e41e5743e2
Ruby <ruby10387@gmail.com>
2017-12-12 16:29:50 +0800

    GameTester

--- src/com/ruby/GameTester.java
+++ src/com/ruby/GameTester.java
@@ -8,27 +8,61 @@ import java.util.Scanner;
 public class GameTester {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		int n = -1;
+		int hp = 100;
+		int pos = 0;
+		int row = 3;
+		int col = 5;
 		while(n != 0){
+			System.out.println("目前位置:"+pos+" "+"hp:"+hp);
 			System.out.print("請輸入方向:");
+			Scanner scanner = new Scanner(System.in);
 			String line = scanner.nextLine();
 			n = Integer.parseInt(line);
+			
 			switch(n){
 			case 2:
 				System.out.println("向下");
+				if(pos/col < row-1){
+					pos = pos+col;
+					hp = hp-5;
+				}else{
+					hp = hp-30;
+				}
 				break;
 			case 4:
 				System.out.println("向左");
+				if(pos%col != 0){
+					pos = pos-1;
+					hp = hp-5;
+				}else{
+					hp = hp-30;
+				}
 				break;
 			case 6:
 				System.out.println("向右");
+				if(pos%col != col-1){
+					pos = pos+1;
+					hp = hp-5;
+				}else{
+					hp = hp-30;
+				}
 				break;
 			case 8:
 				System.out.println("向上");
+				if(pos/col > 0){
+					pos = pos-col;
+					hp = hp-5;
+				}else{
+					hp = hp-30;
+				}
 				break;
 			case 0:
 				System.out.println("Bye");
+				break;
+			}
+			if(hp <= 0){
+				System.out.println("Game over!");
 				break;
 			}
 		}
--- src/com/ruby/oo/Student.java
+++ src/com/ruby/oo/Student.java
@@ -1,19 +1,28 @@
 package com.ruby.oo;
 
 public class Student {
-//	屬性(property,field)在此類別接可用
+//	屬性(property或field)在此類別接可用
+//	instance variable
 //  方法(method)
 	int english;
 	int math;
 	int chinese;
 	String name;
-//	建構子(constructor)方法和類別名稱相同
-	public Student (String n, int e, int m, int c){
-		english = e;
-		math = m;
-		chinese = c;
-		name = n;
+	
+//	建構子(constructor),方法和類別名稱相同
+//	vocal variable
+	public Student (String name, int english, int math, int chinese){
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+		this.name = name;
 	}
+	
+	public Student (String name){
+		this.name = name;
+	}
+//	property和vocal variable名稱可相同,但為不同的資料
+//	this可用在property和method
 	public void print(){
 		System.out.println(english+"\t"+math+"\t"+chinese);
 	}
