ca63d65c1b62f7fa4015421f619d07d4bee47224
Ruby <ruby10387@gmail.com>
2018-03-06 16:31:35 +0800

    Light

---
+++ src/com/java2/object/Light.java
@@ -0,0 +1,14 @@
+package com.java2.object;
+
+public class Light {
+	int price;
+	String brightness;
+	int power;
+	public Light (int price,String brightness,int power) {
+		this.price = price;
+		this.brightness = brightness;
+		this.power = power;
+	}
+	
+	
+}
