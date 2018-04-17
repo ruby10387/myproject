1904480979ce5a2284bf3d6d39515e7fdc10094f
Ruby <ruby10387@gmail.com>
2018-04-17 16:10:58 +0800

    Sogo_2,Sales

--- src/com/java2/io/Sogo.java
+++ src/com/java2/io/Sogo.java
@@ -1,10 +1,20 @@
 package com.java2.io;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.channels.ShutdownChannelGroupException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import com.ruby.shop.Customer;
+import com.ruby.shop.GoldenCustomer;
+import com.ruby.shop.SilverCustomer;
 
 public class Sogo {
 	Scanner scanner = new Scanner(System.in);
@@ -20,6 +30,47 @@ public class Sogo {
 			switch(function){
 			case 1:
 				inputSales();
+				break;
+			case 2:
+				List<Sales> list = new ArrayList<>();
+				try {
+					FileInputStream fis = new FileInputStream("sales.txt");
+					InputStreamReader isr = new InputStreamReader(fis);
+					BufferedReader in = new BufferedReader(isr);
+					String line = in.readLine();
+					while (line != null) {
+						String[] token = line.split("\t");
+						try {
+							int type = Integer.parseInt(token[0]);
+							int amount = Integer.parseInt(token[1]);
+							Sales sales = new Sales(type, amount);
+							list.add(sales);
+						} catch (NumberFormatException e) {
+							System.out.println("資料格式錯誤");
+							return;
+						}
+						line = in.readLine();
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				for (Sales sales: list){
+					Customer customer = null;
+					switch(sales.type){
+					case 1: 
+						customer = new Customer(sales.getAmount());
+						break;
+					case 2:
+						customer = new SilverCustomer(sales.getAmount());
+						break;
+					case 3:
+						customer = new GoldenCustomer(sales.getAmount());
+						break;
+					}
+					customer.print();
+				}
 				break;
 			case 3:
 				return;
