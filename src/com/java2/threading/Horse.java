fa42e4ebaf508997cff33e35abda88138020753c
Ruby <ruby10387@gmail.com>
2018-05-15 16:30:36 +0800

    ArrayList

--- src/com/java2/threading/Horse.java
+++ src/com/java2/threading/Horse.java
@@ -1,6 +1,13 @@
 package com.java2.threading;
 
+import java.util.List;
+
 public class Horse extends Thread{
+	List<Horse> list;
+	public Horse(List<Horse> list){
+		this.list = list;
+	}
+	
 	@Override
 	public void run(){
 		for(int i=0;i<=100;i++){
@@ -13,5 +20,6 @@ public class Horse extends Thread{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		list.add(this);
 	}
 }
--- src/com/java2/threading/Racing.java
+++ src/com/java2/threading/Racing.java
@@ -7,23 +7,35 @@ public class Racing {
 
 	public static void main(String[] args) {
 		Horse h1 = new Horse();
+		Horse h2 = new Horse();
+		Horse h3 = new Horse();
 		h1.start();
-		
-//		Horse h2 = new Horse();
-//		h2.start();
-		
-		HorseRunable h2 = new HorseRunable();
-		Thread thr = new Thread(h2);
-		thr.start();
+		h2.start();
+		h3.start();
 		try {
 			h1.join();
-			thr.join();
+			h2.join();
+			h3.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		System.out.println("main ended");
+		
+//		Horse h1 = new Horse();
+//		h1.start();
+//		HorseRunable h2 = new HorseRunable();
+//		Thread thr = new Thread(h2);
+//		thr.start();
+//		try {
+//			h1.join();
+//			thr.join();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		
 		
 //		List <Horse> horses = new ArrayList<>();
 //		for(int i=0;i<8;i++){
