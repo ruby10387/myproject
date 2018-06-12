3cd736d7b08ff8cc20cadf9cdeb195ae0d52ec3b
Ruby <ruby10387@gmail.com>
2018-06-12 16:29:43 +0800

    schedule

---
+++ src/com/java2/schedule/Course.java
@@ -0,0 +1,11 @@
+package com.java2.schedule;
+
+public class Course {
+	String id;
+	String subject;
+	int day;
+	int time;
+	int duration;
+	
+	
+}
---
+++ src/com/java2/schedule/Main.java
@@ -0,0 +1,24 @@
+package com.java2.schedule;
+
+public class Main {
+	/* 有一文字檔儲存了課表資料(schedule.txt)
+	 * 請讀取資料後，設計程式可檢查是否有空檔?
+	 * 如:
+	 * 請輸入星期(1-7):2
+	 * 請輸入時(0-24):11
+	 * 【有空檔】
+	 * 請輸入星期(1-7):4
+	 * 請輸入時(0-24):14
+	 * 【有課: C004 English 到 16時 】
+	 */
+	
+	public void on(){
+		System.out.print("請輸入星期:");
+		System.out.print("請輸入時:");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
