e9df1a46d43cce2ee56fcb99b6caf15346567f1f
Ruby <ruby10387@gmail.com>
2018-06-26 11:07:47 +0800

    vending

--- src/com/java2/oo/VendingMachine.java
+++ src/com/java2/oo/VendingMachine.java
@@ -2,17 +2,26 @@ package com.java2.oo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
 	int total = 0;
 	List<Drink> drinks = new ArrayList<>();
 	public void on(){
 		System.out.println("請投入硬幣");
-		for(int i=0; i<=2; i++){
-			drinks.get(i);
-			System.out.println();
+		for(int i=0; i<drinks.size(); i++){
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
 		}
-		
+		Scanner scanner = new Scanner(System.in);
+		int data = -1;
+		while (data != 0){
+			data = scanner.nextInt();
+			switch(data){
+			case 0:
+				break;
+			}
+		}
 	}
 
 }
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -14,7 +14,14 @@ public class VendingMain {
 			fr = new FileReader("vending.txt");
 			BufferedReader in = new BufferedReader(fr);
 			String line = in.readLine();
-			String tokens[] = line.split(",");
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for (int i = 0; i < count; i++){
+				String name = tokens[i*2+1];
+				int price = Integer.parseInt(tokens[i*2+2]);
+				vm.drinks.add(new Drink(i+1, name, price));
+			}
+			vm.on();
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -23,8 +30,6 @@ public class VendingMain {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		vm.drinks.add(new Drink(1, "Coke", 25));
-		
 		
 	}
 
